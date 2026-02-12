5ffcae02d069e55f9d31de97d5a347af86028581
Amareen Singh <amanayat@icloud.com>
2026-02-11 20:05:17 -0500

    updated the bot predictor

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,12 +10,19 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
-        max_depth=8,
-        subsample=1,
-        min_samples_leaf=1,
-        random_state=seed
-    )
+    loss="log_loss",
+    learning_rate=0.035,
+    n_estimators=1100,
+    max_depth=4,
+    max_leaf_nodes=18,
+    subsample=0.78,
+    min_samples_leaf=5,
+    min_samples_split=15,
+    max_features="log2",
+    validation_fraction=0.20,
+    n_iter_no_change=30,
+    tol=1e-5,
+    random_state=seed
+)
     model.fit(X, y)
     return model
